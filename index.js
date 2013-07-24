9deeb0619a4e6bd85427a941937bc1915ddbb0d9
Tom MacWright <tom@macwright.org>
2013-07-24 13:54:21 -0400

    Restructure tests, add position assertions

--- index.js
+++ index.js
@@ -1,38 +1,131 @@
-function lint(gj) {
+var jsonlint = require('jsonlint');
+
+function hint(str) {
+
+    var errors = [];
+
     function root(_) {
         if (!_.type) return 'The type member is required and was not found';
         types[_.type](_);
     }
 
-    function FeatureCollection(_) {
+    function requiredProperty(_, name, type) {
+        if (typeof _[name] == 'undefined') {
+            return errors.push({
+                message: '"' + name + '" property required',
+                line: _.__line__
+            });
+        } else if (type === 'array') {
+            if (!Array.isArray(_[name])) {
+                return errors.push({
+                    message: '"' + name +
+                        '" property should be an array, but is an ' +
+                        (typeof _[name]) + ' instead',
+                    line: _.__line__
+                });
+            }
+        } else if (type && typeof _[name] !== type) {
+            return errors.push({
+                message: '"' + name +
+                    '" property should be ' + (type) +
+                    ', but is an ' + (typeof _[name]) + ' instead',
+                line: _.__line__
+            });
+        }
     }
 
+    function FeatureCollection(_) {
+        if (!requiredProperty(_, 'features', 'array')) {
+            _.features.forEach(Feature);
+        }
+    }
+
+    // http://geojson.org/geojson-spec.html#positions
+    function position(_) {
+        if (!Array.isArray(_)) {
+            return errors.push({
+                message: 'position should be an array, is a ' + (typeof _) +
+                    ' instead',
+                line: _.__line__
+            });
+        } else {
+            if (_.length < 2) {
+                return errors.push({
+                    message: 'position must have 2 or more elements',
+                    line: _.__line__
+                });
+            }
+            if (_.some(function(p) {
+                return (typeof p !== 'number');
+            })) {
+                return errors.push({
+                    message: 'each element in a position must be a number',
+                    line: _.__line__
+                });
+            }
+        }
+    }
+
+    // http://geojson.org/geojson-spec.html#point
     function Point(_) {
+        if (!requiredProperty(_, 'coordinates', 'array')) {
+            position(_.coordinates);
+        }
     }
 
     function Polygon(_) {
+        requiredProperty(_, 'coordinates', 'array');
     }
 
     function MultiPolygon(_) {
+        requiredProperty(_, 'coordinates', 'array');
     }
 
     function LineString(_) {
+        requiredProperty(_, 'coordinates', 'array');
     }
 
     function MultiLineString(_) {
+        requiredProperty(_, 'coordinates', 'array');
     }
 
+    // http://geojson.org/geojson-spec.html#multipoint
     function MultiPoint(_) {
+        requiredProperty(_, 'coordinates', 'array');
+    }
+
+    function GeometryCollection(_) {
+        if (!requiredProperty(_, 'geometries', 'array')) {
+            _.geometries.forEach(root);
+        }
+    }
+
+    function Feature(_) {
+        requiredProperty(_, 'properties', 'object');
+        requiredProperty(_, 'geometry', 'object');
     }
 
     var types = {
         Point: Point,
+        Feature: Feature,
         MultiPoint: MultiPoint,
         LineString: LineString,
+        MultiLineString: MultiLineString,
         FeatureCollection: FeatureCollection,
+        GeometryCollection: GeometryCollection,
         Polygon: Polygon,
         MultiPolygon: MultiPolygon
     };
 
-    return root(gj);
+    try {
+        gj = jsonlint.parse(str);
+    } catch(e) {
+        return e;
+    }
+
+    root(gj);
+
+    return errors;
 }
+
+module.exports.hint = hint;
--- test/hint.test.js
+++ test/hint.test.js
@@ -1,6 +1,31 @@
-var expect = require('expect.js');
+var expect = require('expect.js'),
+    fs = require('fs'),
+    glob = require('glob'),
+    geojsonhint = require('../');
+
+function file(x) {
+    return fs.readFileSync(x, 'utf8');
+}
+
+function filejs(x) {
+    return JSON.parse(fs.readFileSync(x, 'utf8'));
+}
 
 describe('geojsonhint', function() {
-    it('validates correct files', function() {
+    describe('validates correct files', function(done) {
+        glob.sync('test/data/good/*.geojson').forEach(function(f) {
+            it('validates ' + f, function() {
+                var gj = file(f);
+                expect(geojsonhint.hint(gj)).to.eql([]);
+            });
+        });
+    });
+    describe('validates incorrect files', function() {
+        glob.sync('test/data/bad/*.geojson').forEach(function(f) {
+            it('invalidates ' + f, function() {
+                var gj = file(f);
+                expect(geojsonhint.hint(gj)).to.eql(filejs(f.replace('geojson', 'result')));
+            });
+        });
     });
 });
